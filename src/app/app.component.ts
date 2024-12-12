bb0cb02730f9c74fe466597b4226605fbcaa102a
Sven Friedrich <128075628+Trouble2701@users.noreply.github.com>
2024-12-12 22:01:29 +0100

    resize site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterOutlet } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from "@angular/router";
 
 @Component({
@@ -16,24 +16,41 @@ import { RouterModule } from "@angular/router";
 export class AppComponent {
   title = 'simpleCRM';
   open = true;
+  siteWidth: number = 1440;
   name: boolean = true;
+  maxWidth: number = 1000;
 
   @ViewChild('drawer') drawer: any;
   @ViewChild('site') site: ElementRef | any;
   @ViewChild('name') siteName: ElementRef | any;
-  
-  constructor(private router: Router){}
 
-  changeWidth(){
-    setTimeout(() => this.site.nativeElement.setAttribute('style', this.drawer.opened == true ? 'max-width:1140px' : 'max-width:1440px'), 250);
+  constructor(private router: Router) {
+    this.siteWidth = window.innerWidth;
+  }
+
+  changeWidth() {
+    setTimeout(() => this.site.nativeElement.setAttribute('style', this.drawer.opened == true ? 'width: calc(100vw - 412px);' : 'width:calc(100vw - 112px);'), 250);
     setTimeout(() => this.siteName.nativeElement.setAttribute('style', this.drawer.opened == true ? 'display: none; transition: all 1000ms ease-in-out;' : 'display: flex; transition: all 1000ms ease-in-out;'), 250);
   }
 
-  changeSite(siteUrl: any){
-      this.site.nativeElement.setAttribute('style', 'transform: translateX(100vw)');
-      setTimeout(() => {
-        this.router.navigate([siteUrl]); 
-      }, 250);
-      setTimeout(() => this.site.nativeElement.setAttribute('style', 'transform: translateX(0)'), 400);
+  setOpenTrue(){
+    if(window.innerWidth < this.maxWidth) this.open = true;
+  }
+
+  changeSite(siteUrl: any) {
+    this.site.nativeElement.setAttribute('style', 'transform: translateX(100vw)');
+    setTimeout(() => this.router.navigate([siteUrl]), 250);
+    setTimeout(() => this.site.nativeElement.setAttribute('style', 'transform: translateX(0)'), 400);
+  }
+
+  @HostListener('window:resize', ['$event.target.innerWidth'])
+  onResize(width: any) {
+    this.siteWidth = width;
+    this.changeSiteWidth();
+  }
+
+  changeSiteWidth(){
+    this.open = this.siteWidth < this.maxWidth ? false : true;
+    this.changeWidth();
   }
 }
